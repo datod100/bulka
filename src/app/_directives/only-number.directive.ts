dab25192087ba91052ddd725a75c937f8a40bffd
Dato Dato <datod100@gmail.com>
2018-02-24 21:07:18 +0200

    order

--- src/app/_directives/only-number.directive.ts
+++ src/app/_directives/only-number.directive.ts
@@ -5,29 +5,56 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class OnlyNumber {
 
-  constructor(private el: ElementRef) { }
+  DIGITS_REGEXP = new RegExp(/\D/g);
+  constructor(private el: ElementRef) {
 
-  @Input() OnlyNumber: boolean;
-
-  @HostListener('keydown', ['$event']) onKeyDown(event) {
-    let e = <KeyboardEvent> event;
-    if (this.OnlyNumber) {
-      if ([46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 ||
-        // Allow: Ctrl+A
-        (e.keyCode == 65 && e.ctrlKey === true) ||
-        // Allow: Ctrl+C
-        (e.keyCode == 67 && e.ctrlKey === true) ||
-        // Allow: Ctrl+X
-        (e.keyCode == 88 && e.ctrlKey === true) ||
-        // Allow: home, end, left, right
-        (e.keyCode >= 35 && e.keyCode <= 39)) {
-          // let it happen, don't do anything
-          return;
+    // Sanatize clipboard by removing any non-numeric input after pasting
+    this.el.nativeElement.onpaste = (e: any) => {
+      e.preventDefault();
+      let text;
+      let clp = (e.originalEvent || e).clipboardData;
+      if (clp === undefined || clp === null) {
+        text = (<any>window).clipboardData.getData('text') || '';
+        if (text !== '') {
+          text = text.replace(this.DIGITS_REGEXP, '');
+          if (window.getSelection) {
+            let newNode = document.createElement('span');
+            newNode.innerHTML = text;
+            window.getSelection().getRangeAt(0).insertNode(newNode);
+          } else {
+            (<any>window).selection.createRange().pasteHTML(text);
+          }
         }
-        // Ensure that it is a number and stop the keypress
-        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-            e.preventDefault();
+      } else {
+        text = clp.getData('text/plain') || '';
+        if (text !== '') {
+          text = text.replace(this.DIGITS_REGEXP, '');
+          document.execCommand('insertText', false, text);
         }
       }
+    };
   }
+
+  @HostListener('keydown', ['$event']) onKeyDown(event) {
+    let e = <KeyboardEvent>event;
+    if ([46, 8, 9, 27, 13, 110, 190].indexOf(e.keyCode) !== -1 ||
+      // Allow: Ctrl+A
+      (e.keyCode === 65 && (e.ctrlKey || e.metaKey)) ||
+      // Allow: Ctrl+C
+      (e.keyCode === 67 && (e.ctrlKey || e.metaKey)) ||
+      // Allow: Ctrl+V
+      (e.keyCode === 86 && (e.ctrlKey || e.metaKey)) ||
+      // Allow: Ctrl+X
+      (e.keyCode === 88 && (e.ctrlKey || e.metaKey)) ||
+      // Allow: home, end, left, right
+      (e.keyCode >= 35 && e.keyCode <= 39)) {
+      // let it happen, don't do anything
+      return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+      e.preventDefault();
+    }
+  }
+
 }
--- src/app/orders/orders-edit/orders-edit.component.ts
+++ src/app/orders/orders-edit/orders-edit.component.ts
@@ -6,7 +6,30 @@ import { error } from 'selenium-webdriver';
 import { Observable } from 'rxjs/Observable';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { GridApi } from 'ag-grid/dist/lib/gridApi';
-import { GridOptions } from "ag-grid/main";
+import { GridOptions, SelectCellEditor } from "ag-grid/main";
+
+
+function checkKey(event) {
+  let e = <KeyboardEvent>event;
+  if ([46, 8, 9, 27, 13, 110, 190, 17].indexOf(e.keyCode) !== -1 ||
+    // Allow: Ctrl+A
+    (e.keyCode == 65 && e.ctrlKey === true) ||
+    // Allow: Ctrl+V
+    (e.keyCode == 86 && e.ctrlKey === true) ||
+    // Allow: Ctrl+C
+    (e.keyCode == 67 && e.ctrlKey === true) ||
+    // Allow: Ctrl+X
+    (e.keyCode == 88 && e.ctrlKey === true) ||
+    // Allow: home, end, left, right
+    (e.keyCode >= 35 && e.keyCode <= 39)) {
+    // let it happen, don't do anything
+    return;
+  }
+  // Ensure that it is a number and stop the keypress
+  if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+    e.preventDefault();
+  }
+}
 
 @Component({
   selector: 'app-orders-edit',
@@ -17,16 +40,20 @@ import { GridOptions } from "ag-grid/main";
 export class OrdersEditComponent implements OnInit, OnDestroy {
 
   gridOptions: GridOptions;
-  columnDefs: any[]
+  gridOptions2: GridOptions;
+  columnDefs: any[];
+  columnDefs2: any[];
   private context;
-  private frameworkComponents;
+  private context2;
   private gridApi: GridApi;
+  private gridApi2: GridApi;
 
   headerData = [];
+  tableData = [];
   products = [];
   cycles = [];
   headerTableStyle;
-  defaultColDef;
+  tableStyle;
 
   title;
   order: Order;
@@ -53,9 +80,11 @@ export class OrdersEditComponent implements OnInit, OnDestroy {
     this.gridOptions = <GridOptions>{};
     this.gridOptions.domLayout = 'autoHeight'
 
+    this.gridOptions2 = <GridOptions>{};
+    this.gridOptions2.domLayout = 'autoHeight'
+
     this.context = { componentParent: this };
-    this.frameworkComponents = {
-    };
+    this.context2 = { componentParent: this };
 
     this.columnDefs = [
       {
@@ -86,6 +115,62 @@ export class OrdersEditComponent implements OnInit, OnDestroy {
       }
     ];
 
+    this.columnDefs2 = [
+      {
+        headerName: "סמן", field: "select", width: 50, cellClass: "header-bold", editable: false,
+        cellRenderer: params => {
+          return `<input type='checkbox' ${params.value ? 'checked' : ''} />`;
+        },
+        cellStyle: function (params) {
+          if (params.data.cellStyle) {
+            return params.data.cellStyle;
+          } else {
+            return null;
+          }
+        }
+      },
+      {
+        headerName: "סטטוס", field: "status", width: 70, cellClass: "header-bold", editable: false,
+        cellRenderer: params => {
+          return `<input type='checkbox' ${params.value ? 'checked' : ''} />`;
+        },
+        cellStyle: function (params) {
+          if (params.data.cellStyle) {
+            return params.data.cellStyle;
+          } else {
+            return null;
+          }
+        }
+      },
+      {
+        group: "קבוצה", field: "group", width: 120,
+        cellStyle: function (params) {
+          if (params.data.cellStyle) {
+            return params.data.cellStyle;
+          } else {
+            return null;
+          }
+        },
+        editable: true,
+        cellEditor: 'agSelectCellEditor',
+        cellEditorParams: {          
+          values: ['Male', 'Female'],
+          cellRenderer: function (params){
+            return "<span style='color:red'>" + params.value + "</span>";
+          }
+        },
+      },
+      {
+        headerName: "שעת אספקה", field: "supply_time", width: 85, cellClass: "header-bold", editable: false,
+        cellRenderer: function (params) {
+          if (params.value) {
+            let time = params.value;
+            return time.slice(0, -3);
+          }
+        }
+      }
+    ];
+
     this.order = new Order();
     this.order.client = new Client();
     this.order.status = new Status();
@@ -105,25 +190,28 @@ export class OrdersEditComponent implements OnInit, OnDestroy {
 
   private buildHeaderTable() {
     for (let i = 0; i < this.cycles.length; i++) {
-      this.headerData.push({ cycle: this.cycles[i].name, ready_time: this.cycles[i].cycle_time, cellStyleRow: { backgroundColor: '#ffc1074a' } });
+      this.headerData.push({ cycle: this.cycles[i].name, ready_time: this.cycles[i].cycle_time, cellStyleRow: { backgroundColor: '#ffc1074a', userSelect: 'text' } });
     }
-    this.headerData.push({ cycle: "סיכום הזמנות", colSpan: 2, disableEdit:true, cellStyle: { left: 'auto' }, cellStyleRow: { backgroundColor: '#ccccccbd' } });
-    this.headerData.push({ cycle: "נשלח", colSpan: 2,disableEdit:true, cellStyle: { left: 'auto' }, cellStyleRow: { backgroundColor: '#ccccccbd' } });
-    this.headerData.push({ cycle: "יתרת סחורה במאפייה", colSpan: 2,disableEdit:true, cellStyle: { left: 'auto' }, cellStyleRow: { backgroundColor: '#ccccccbd' } });
+    this.headerData.push({ cycle: "סיכום הזמנות", colSpan: 2, disableEdit: true, cellStyle: { left: 'auto' }, cellStyleRow: { backgroundColor: '#ccccccbd', userSelect: 'text' } });
+    this.headerData.push({ cycle: "נשלח", colSpan: 2, disableEdit: true, cellStyle: { left: 'auto' }, cellStyleRow: { backgroundColor: '#ccccccbd', userSelect: 'text' } });
+    this.headerData.push({ cycle: "יתרת סחורה במאפייה", colSpan: 2, disableEdit: true, cellStyle: { left: 'auto' }, cellStyleRow: { backgroundColor: '#ccccccbd', userSelect: 'text' } });
 
     for (let k = 0; k < this.products.length; k++) {
       let colWidth = this.products[k].name.length * 9;
 
       for (let i = 0; i < this.cycles.length; i++) {
-        this.headerData[i]["product"+this.products[k].product_id]='';
+        this.headerData[i]["product" + this.products[k].product_id] = '';
       }
 
       this.columnDefs.push({
         headerName: this.products[k].name,
-        field: "product"+this.products[k].product_id,
-        editable: function(params){
+        field: "product" + this.products[k].product_id,
+        editable: function (params) {
           if (params.data.disableEdit) return false
           return true;
+        },
+        suppressKeyboardEvent: function (params) {
+          ///checkKey(params.event);
         },
         width: (colWidth < 70) ? 70 : colWidth, cellStyle: function (params) {
           if (params.data.cellStyleRow) {
@@ -137,22 +225,50 @@ export class OrdersEditComponent implements OnInit, OnDestroy {
     this.gridApi.setColumnDefs(this.columnDefs);
     this.calcGridWidth();
     this.gridApi.setRowData(this.headerData);
-    
+
+    this.headerTableCalc();
   }
 
+
+  private buildTable() {
+    this.tableData.push({ select: true, status: false, supply_time: "06:00:00" });
+    this.gridApi2.setColumnDefs(this.columnDefs2);
+    this.calcGridWidth();
+    this.gridApi2.setRowData(this.tableData);
+
+  }
+
+  headerTableCellValueChanged(params) {
+    if (isNaN(+params.value)) {
+      params.data[params.colDef.field] = null;
+    }
+    params.context.componentParent.headerTableCalc();
+  }
+
+  headerTableCalc() {
+    let rowOffset = this.cycles.length;
+    for (let k = 0; k < this.products.length; k++) {
+      this.headerData[rowOffset]["product" + this.products[k].product_id] = 0;
+      this.headerData[rowOffset + 1]["product" + this.products[k].product_id] = 0;
+      for (let i = 0; i < this.cycles.length; i++) {
+        this.headerData[rowOffset]["product" + this.products[k].product_id] += +this.headerData[i]["product" + this.products[k].product_id];
+      }
+
+      this.headerData[rowOffset + 2]["product" + this.products[k].product_id] = this.headerData[rowOffset]["product" + this.products[k].product_id] - this.headerData[rowOffset + 1]["product" + this.products[k].product_id];
+    }
+    this.gridApi.setRowData(this.headerData);
+  }
 
   onGridReady(params) {
     this.gridApi = params.api;
 
     Observable.forkJoin(
-      this.clientService.getAll(),
       this.statusesService.getCycles(),
       this.productService.getAll()
     ).subscribe(
       data => {
-        this.clients = data[0];
-        this.cycles = data[1];
-        this.products = data[2];
+        this.cycles = data[0];
+        this.products = data[1];
 
         this.buildHeaderTable();
 
@@ -187,23 +303,70 @@ export class OrdersEditComponent implements OnInit, OnDestroy {
     //this.gridApi.resetRowHeights();
   }
 
+  onGridReady2(params) {
+    this.gridApi2 = params.api;
+
+    Observable.forkJoin(
+      this.clientService.getAll()
+    ).subscribe(
+      data => {
+        this.clients = data[0];
+
+        this.buildTable();
+
+      },
+      err => console.error(err),
+      () => {
+        if (this.order_id) {
+          this.title = "Edit";
+
+          Observable.forkJoin(
+            this.ordersService.getById(this.order_id),
+            this.ordersService.getItemsById(this.order_id)
+          ).subscribe(
+            data => {
+              this.order = data[0][0];
+              this.order.items = data[1];
+              this.order.client = this.clients.find(item => item.client_id === this.order.client_id);
+              if (this.order.confirmation_date) this.order.confirmation_date = new Date(this.order.confirmation_date);
+              if (this.order.supply_date) this.order.supply_date = new Date(this.order.supply_date);
+            },
+            err => console.error(err)
+          );
+
+        } else {
+          this.title = "Create";
+        }
+
+      }
+    );
+
+  }
+
   private calcGridWidth() {
-    let newWidth = 0;
     let screenWidth = window.outerWidth;
+
+    let newWidth = 0;
     for (let i = 0; i < this.columnDefs.length; i++) {
       newWidth += this.columnDefs[i].width;
     }
     newWidth += 2;
-
     this.headerTableStyle = { width: ((newWidth > screenWidth) ? screenWidth : newWidth) + 'px' };
     this.gridApi.doLayout();
+
+    newWidth = 0;
+    for (let i = 0; i < this.columnDefs2.length; i++) {
+      newWidth += this.columnDefs2[i].width;
+    }
+    newWidth += 2;
+    this.tableStyle = { width: ((newWidth > screenWidth) ? screenWidth : newWidth) + 'px' };
+    this.gridApi2.doLayout();
   }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.order_id = +params['id'];
       var action = params['action'];
-
 
     });
   }
